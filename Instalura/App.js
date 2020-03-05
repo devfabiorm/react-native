8aca6f986e1dcaa810c70a562d8990a6ceb7decb
yagamikisuke <fabio.evangelion.guitar@gmail.com>
2020-03-05 16:50:04 -0300

    Instalacao e inicializacao do projeto

---
+++ Instalura/App.js
@@ -0,0 +1,16 @@
+
+import React, {Fragment} from 'react';
+import { Text } from 'react-native';
+
+
+const App = () => {
+  return (
+    
+      <Fragment>
+        <Text>Marina</Text>
+        <Text>Fernando</Text>
+      </Fragment>
+  );
+};
+
+export default App;
