46b46173a89b56aa68c9adcad3eb879101ff09a9
yagamikisuke <fabio.evangelion.guitar@gmail.com>
2020-03-09 16:09:10 -0300

    Usando TextInput para inserir comentarios

--- InstaAlura/src/Components/Comentarios/Comentarios.js
+++ InstaAlura/src/Components/Comentarios/Comentarios.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import {FlatList, Text} from 'react-native';
+import React, { Fragment, useState } from 'react';
+import { FlatList, Text, TextInput, Image, View, TouchableOpacity } from 'react-native';
+import estilo from './estilo';
 
-const Comentarios = ({comentarios}) => {
+const Comentarios = ({ comentarios }) => {
+
+    const [estComentarios, setComentarios] = useState(comentarios);
+
+    const comentar = () => {
+
+        const novoComentario = {
+            date: Date.Now,
+            text: conteudoCampoInput,
+            userName: "Fabio"
+        }
+        setComentarios([...estComentarios, novoComentario]);
+        refTextInput.clear();
+    }
+
+    let conteudoCampoInput = "";
+    let refTextInput = "";
 
     return (
-        <FlatList 
-        data={comentarios}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({item}) => <Text>{item.text}</Text>}
-        />
+
+        <Fragment>
+            <FlatList
+                data={estComentarios}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => 
+                <View style={estilo.naMesmaLinha}>
+                    <Text>{item.userName} </Text>
+                    <Text> {item.text}</Text>
+                </View>
+                }
+            />
+            <View style={estilo.naMesmaLinha}>
+                <TextInput ref={referencia => refTextInput = referencia} placeholder={"Deixe seu comentários..."} style={{ flex: 1 }} onChangeText={texto => conteudoCampoInput = texto} />
+                <TouchableOpacity onPress={comentar}>
+                    <Image source={require("../../../res/img/send.png")} style={estilo.botaoEnviar} />
+                </TouchableOpacity>
+            </View>
+        </Fragment>
     );
 };
 
